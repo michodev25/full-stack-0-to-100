adabaf0015fddcaca22fae89208c987e16733ad4
michodev <mikemaggi71@gmail.com>
2025-09-03 21:18:57 -0400

    curso de midudev again

---
+++ parte-uno/parte-uno-react/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Message from './components/Message.tsx';
+function App() {
+
+  return (
+  <div className='App'>
+     <Message message='Hello world'/>
+      <Message message='Recordando React'/>
+  </div>
+  )
+}
+
+export default App
---
+++ parte-uno/parte-uno-react/src/components/Message.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Message(props: { message: string }) {
+  return (
+    <h1>{props.message}</h1>
+  )
+}
+
+export default Message
