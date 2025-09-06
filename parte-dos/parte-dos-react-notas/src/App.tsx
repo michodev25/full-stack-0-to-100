3952526e703060542ac34d60738f2823704ebd2e
michodev <mikemaggi71@gmail.com>
2025-09-06 17:32:32 -0400

    showall-part2

--- parte-dos/parte-dos-react-notas/src/App.tsx
+++ parte-dos/parte-dos-react-notas/src/App.tsx
@@ -6,18 +6,16 @@ import { Nota } from './components/Nota.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [notesState, setNotesState] = useState(notes)
-  const [newNote, setNewNote] = useState('')
+  const [notesState, setNotesState] = useState(notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleChange = (event:any) => {
     setNewNote(event.target.value)
   }
    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = (event:any) => {
-     event.preventDefault();
-  }
-  const handleClick = () => {
-   const noteToAdd = {
+     const noteToAdd = {
     id: notesState.length + 1,
     content: newNote,
     date: new Date().toISOString().split('T')[0],
@@ -25,21 +23,30 @@ function App() {
    }
     setNotesState(notesState.concat(noteToAdd))
     setNewNote("")
-
+     event.preventDefault();
   }
+  const handleShowAll = () => {
+    setShowAll(()=> !showAll);
+  }
+
   if (notesState.length === 0 || !notesState) return <h1>No hay notas</h1>
   return (
     <div className="div">
       <h1>Notes</h1>
+      <button onClick={handleShowAll}>{showAll ? "Show only important" : "Show all " }</button>
       <ol>
-        {notesState.map(note => (
+        {notesState.filter( note => {
+          if(showAll) return note;
+          return note.important === true;
+        })
+        .map(note => (
           <Nota key={note.id} {...note} />
         ))}
       </ol>
 
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} value={newNote} />
-        <button type='submit' onClick={handleClick}>Crear Nota</button>
+        <button>Crear Nota</button>
       </form>
 
     </div>
