8a88229faac7fbb419985d3eeb7f6a936f41fc83
michodev <mikemaggi71@gmail.com>
2025-09-08 22:05:17 -0400

    parte-tres-useeffect-ok

--- parte-tres/bootcamp-react-fetch-mutacion/src/App.jsx
+++ parte-tres/bootcamp-react-fetch-mutacion/src/App.jsx
@@ -1,28 +1,40 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Note from './components/Note';
 import './App.css'
 
 function App() {
-  
+
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [loading, setLoading] = useState(false);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect((
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((json))
-  ), [])
+      .then((res) => res.json())
+      .then((json) => {setNotes(json)})
+  ), []);
 
   return (
-    <h1>Notes</h1>
-    {loading ? "Cargando..." : ""}
-    <ol>
-      {notes.map}
+    <div> <h1>Notes</h1>
+      {loading ? "Cargando..." : ""}
+      <ol>
+        {notes.map(note => {
+          <Note key={note.id} {...note} />
+        }
+        )}
+      </ol>
 
-    </ol>
-   
+      <form>
+        
+      </form>
+
+    </div>
+
+
   )
 }
 
