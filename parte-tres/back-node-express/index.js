7c2e03697a8e21826a2a744048724f2f85cc37a4
michodev <mikemaggi71@gmail.com>
2025-09-13 12:28:42 -0400

    recordando-express

--- parte-tres/back-node-express/index.js
+++ parte-tres/back-node-express/index.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo");
+import express from 'express';
+const app = express();  
+
+app.get('/', (req, res) => {(
+
+    res.send("Reviviendo desde Express")
+
+)})
+
+app.listen(3000 || process.env.PORT, () => {
+    console.log("Server is running on port"+`${process.env.PORT}`)
+})
