c0b7232182d7539f7c53ac4ccdeec79d3c6f7ba7
michodev <mikemaggi71@gmail.com>
2025-09-27 15:17:59 -0400

    rutas y links

--- parte-cuatro/notes/src/App.jsx
+++ parte-cuatro/notes/src/App.jsx
@@ -1,37 +1,24 @@
-import { useState } from 'react'
+
 import { BrowserRouter, Routes, Route, Link } from "react-router";
 import Notes from './components/Notes'
 import Users from './components/Users'
 import './App.css'
 
 function App() {
-  const [page, setPage] = useState(() => {
-    const { pathname } = window.location
-    const page = pathname.slice(1)
-    console.log(page)
-    return page
-  })
 
-  const getContent = () => {
-    if (page === 'users') return <Users />
-    else if (page === 'notes') return <Notes />
-    else return <h1>Home</h1>
-  }
-
-  const toPage = page => event => {
-    event.preventDefault()
-   window.history.pushState(null, '', `/${page}`)
-   setPage(page)
-  }
-const inlineStyle = { padding: '10px' }
+  const inlineStyle = { padding: '10px' }
   return (
     <BrowserRouter>
       <header>
-       <Link to="#" onClick={toPage('home')} style={inlineStyle}> Home</Link>
-        <Link to="#" onClick={toPage('notes')} style={inlineStyle}>Notes </Link>
-        <Link to="#" onClick={toPage('users')} style={inlineStyle}> Users</Link>
+        <Link to="/home" style={inlineStyle}> Home</Link>
+        <Link to="/notes" style={inlineStyle}>Notes </Link>
+        <Link to="/users" style={inlineStyle}> Users</Link>
       </header>
-      {getContent()}
+      <Routes>
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </BrowserRouter>
   )
 }
