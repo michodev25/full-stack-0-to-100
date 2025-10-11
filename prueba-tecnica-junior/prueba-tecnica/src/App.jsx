2c6d22d69d45ad8dad9f14df6ff6c7ca54f54cd9
michodev <mikemaggi71@gmail.com>
2025-10-11 16:18:50 -0400

    prueba preuba

--- prueba-tecnica-junior/prueba-tecnica/src/App.jsx
+++ prueba-tecnica-junior/prueba-tecnica/src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
+import './style.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_RANDOM_IMAGE = `https://cataas.com/cat/cute/says/${firstThreeWords}?json=true`
 function App () {
-  const [fact, setFact] = useState('')
+  const [fact, setFact] = useState()
+  const [catImg, setCatImg] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -12,13 +14,13 @@ function App () {
         const { fact } = data
         setFact(fact)
 
-        const firstThreeWords = fact.split(' ', 3)
+        const firstThreeWords = fact.split(' ', 3).join(' ')
         console.log(firstThreeWords)
         fetch(`https://cataas.com/cat/cute/says/${firstThreeWords}?json=true`)
           .then(res => res.json())
           .then(data => {
             const { url } = data
-            console.log(url)
+            setCatImg(url)
           })
       })
   }, [])
@@ -26,7 +28,8 @@ function App () {
   return (
     <main>
       <h1>App de gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {catImg && <img src={catImg} alt={`esta es una imagen de un gato con las palabras ${fact}`} />}
     </main>
 
   )
