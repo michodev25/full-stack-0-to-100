ab4d6e5440c9ace6dbebafa5fdcf92e102162849
michodev <mikemaggi71@gmail.com>
2025-10-15 13:33:41 -0400

    loading

--- proyecto-peliculas-buscar/prueba-buscador-peliculas/src/App.jsx
+++ proyecto-peliculas-buscar/prueba-buscador-peliculas/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 function App() {
   const { search, updateSearch, error } = useSearch()
-  const { movies, getMovies } = useMovies({ search })
+  const { movies, getMovies, loading } = useMovies({ search })
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -24,16 +26,22 @@ function App() {
             style={{
               border: "1 px solid transparent",
               borderColor: error ? 'red' : 'transparent'
-          } }
-          onChange={handleChange} value={search} 
-          placeholder='Avengers, Star Wars, The Matrix' />
+            }}
+            onChange={handleChange} value={search}
+            placeholder='Avengers, Star Wars, The Matrix' />
           <button type='submit'>Buscar</button>
         </form>
-         {error ? <p style={{color:"red"}}>{error}</p> : null}
+        {error ? <p style={{ color: "red" }}>{error}</p> : null}
       </header>
 
       <main>
-        <Movies movies={movies} />
+        {
+          loading ?
+            <Box sx={{ display: 'flex' }}>
+              <CircularProgress />
+            </Box>
+            : <Movies movies={movies} />
+        }
       </main>
     </div>
   )
--- proyecto-peliculas-buscar/prueba-buscador-peliculas/src/hooks/useMovies.js
+++ proyecto-peliculas-buscar/prueba-buscador-peliculas/src/hooks/useMovies.js
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import { searchMovies } from '../services/movies'
+import { useRef, useState } from 'react'
+import { searchMovies } from '../services/moviesServices.js'
 
 export function useMovies({ search }) {
     const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const previusSearch = useRef(search)
 
-    const getMovies = () => {
-        const newMovies = searchMovies({ search })
+    const getMovies = async () => {
+        if (previusSearch.current === search) return
+        try {
+        setLoading(true)
+        setError(null)
+        previusSearch.current = search
+        const newMovies = await searchMovies({ search })
         setMovies(newMovies)
+        } catch (error) {
+            setError(error.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
 
-    return { movies, getMovies }
+    return { movies, getMovies, loading, error }
 }
