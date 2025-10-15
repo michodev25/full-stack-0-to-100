7cc6e6a5517a4816771aea6c8b2c864cd563af5e
michodev <mikemaggi71@gmail.com>
2025-10-15 14:08:17 -0400

    hooks-useMemo- useCallback

--- proyecto-peliculas-buscar/prueba-buscador-peliculas/src/App.jsx
+++ proyecto-peliculas-buscar/prueba-buscador-peliculas/src/App.jsx
@@ -1,21 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
 
 function App() {
+  const [sort,setSort] = useState(false)
   const { search, updateSearch, error } = useSearch()
-  const { movies, getMovies, loading } = useMovies({ search })
+  const { movies, getMovies, loading } = useMovies({ search, sort })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({ search})
     // Lógica para manejar la búsqueda
   }
   const handleChange = (event) => {
-    updateSearch(event.target.value)
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    getMovies({ search: newSearch })
+  }
+
+  const handleSort = (event) => {
+    event.preventDefault()
+    setSort(!sort)
   }
   return (
     <div className='page'>
@@ -31,6 +41,7 @@ function App() {
             placeholder='Avengers, Star Wars, The Matrix' />
           <button type='submit'>Buscar</button>
         </form>
+       <Switch onChange={handleSort}/>
         {error ? <p style={{ color: "red" }}>{error}</p> : null}
       </header>
 
--- proyecto-peliculas-buscar/prueba-buscador-peliculas/src/hooks/useMovies.js
+++ proyecto-peliculas-buscar/prueba-buscador-peliculas/src/hooks/useMovies.js
@@ -1,27 +1,30 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useMemo, useCallback } from 'react'
 import { searchMovies } from '../services/moviesServices.js'
 
-export function useMovies({ search }) {
+export function useMovies({ search, sort }) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const previusSearch = useRef(search)
 
-    const getMovies = async () => {
+    const sortedMovies = useMemo(() => {
+        return sort ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) : movies
+    }, [sort, movies])
+
+    const getMovies = useCallback(async ({ search }) => {
         if (previusSearch.current === search) return
         try {
-        setLoading(true)
-        setError(null)
-        previusSearch.current = search
-        const newMovies = await searchMovies({ search })
-        setMovies(newMovies)
+            setLoading(true)
+            setError(null)
+            previusSearch.current = search
+            const newMovies = await searchMovies({ search })
+            setMovies(newMovies)
         } catch (error) {
             setError(error.message)
         } finally {
             setLoading(false)
         }
-    }
+    }, [])
 
-
-    return { movies, getMovies, loading, error }
+return { movies: sortedMovies, getMovies, loading, error }
 }
